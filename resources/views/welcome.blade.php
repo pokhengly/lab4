930944364fe31d9c334c1cd9261a1fb538f319ba
pokhengly <henglypok21012021@gmail.com>
2023-02-28 22:41:57 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initialscale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Royal University of Phnom Penh</h1>
+    <h1>Class: M2</h1>
+    <h1>Name: Pok Hengly</h1>
+    <h1>Contact App</h1>
+<div>
+    <a href='{{ route('contacts.index') }}'>All contacts</a>
+</div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',function(){
+    return view ('welcome');
+});
+Route::get('/contacts', function () {
+    return view('contacts.index');
+})->name('contacts.index');
+Route::get('/contacts/create', function () {
+    return view('contacts.create');
+})->name('contacts.create');
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact " . $id;
+})->name('contacts.show');
+
+
